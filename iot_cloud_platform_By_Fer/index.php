06c8e2faf62672b1e7c3f521d53ae8fef9b9fa91
Calimero_Maker <fernando.tob.per@techtalents.club>
2018-02-24 13:32:40 +0100

    Basic structure setup
    Index.php linked with js and css files.

---
+++ iot_cloud_platform_By_Fer/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>iot Cloud platform</title>
+    <link rel = "stylesheet" href="styles/style.css">
+    <script src = "scripts/script.js"></script>
+    
+</head>
+
+<body>
+    <h1 class="titulo"> Hola mundo: </h1>
+</body>
+</html>
